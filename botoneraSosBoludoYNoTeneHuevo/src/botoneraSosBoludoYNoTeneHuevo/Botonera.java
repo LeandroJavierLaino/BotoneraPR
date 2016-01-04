610826c0ea0440dc6717d87c3ce9150ceb9f14b6
Leandro Javier Laiño <leanlaino@gmail.com>
2016-01-04 20:48:25 -0300

    Se agregan audios y mas botones

--- botoneraSosBoludoYNoTeneHuevo/src/botoneraSosBoludoYNoTeneHuevo/Botonera.java
+++ botoneraSosBoludoYNoTeneHuevo/src/botoneraSosBoludoYNoTeneHuevo/Botonera.java
@@ -89,11 +89,18 @@ public class Botonera extends JFrame implements ActionListener{
 	 * 
 	 */
 	private static final long serialVersionUID = -8287199945932537354L;
-	JButton boton1 = new JButton("Tada");
-	JButton boton2 = new JButton("Tada2");
+	JButton boton1 = new JButton("Tomatela te dije");
+	JButton boton2 = new JButton("Maiameee");
+	JButton boton3 = new JButton("No comments");
+	JButton boton4 = new JButton("Para que te traje");
+	JButton boton5 = new JButton("So boludo y no tene huevos");
+	JButton boton6 = new JButton("Ese es el equilatero");
+	JButton boton7 = new JButton("Puede darme dinero");
+	JButton boton8 = new JButton("Basta Chicos");
+	JButton boton9 = new JButton("Aguantaaaa");		
     public Botonera(){
-        super("Pulse el boton");
-        setSize(400,100);
+        super("Botonera PR");
+        setSize(400,400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
@@ -101,9 +108,23 @@ public class Botonera extends JFrame implements ActionListener{
         boton1.addActionListener(this);
         add(boton2);
         boton2.addActionListener(this);
+        add(boton3);
+        boton3.addActionListener(this);
+        add(boton4);
+        boton4.addActionListener(this);
+        add(boton5);
+        boton5.addActionListener(this);
+        add(boton6);
+        boton6.addActionListener(this);
+        add(boton7);
+        boton7.addActionListener(this);
+        add(boton8);
+        boton8.addActionListener(this);
+        add(boton9);
+        boton9.addActionListener(this);         
         setVisible(true);
     }
-    public static void main(String[] Dar10){
+    public static void main(String[] args){
         new Botonera();
     }
     public void ReproducirSonido(String nombreSonido){
@@ -119,9 +140,23 @@ public class Botonera extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == boton1){
-            ReproducirSonido("sounds/tada.wav");
+            ReproducirSonido("sounds/Me_importa_un_carajo_tomatel_te_dije.wav");
         }if(e.getSource() == boton2){
-        	ReproducirSonido("sounds/tada.wav");
+        	ReproducirSonido("sounds/MAIAMEEE_-_Ricardo_Fort_Jus.wav");
+        }if(e.getSource() == boton3){
+        	ReproducirSonido("sounds/Coco_Basile_No_comments.wav");
+        }if(e.getSource() == boton4){
+        	ReproducirSonido("sounds/Para_que_te_traje_Vicente_-_Bambino_Pons.wav");
+        }if(e.getSource() == boton5){
+        	ReproducirSonido("sounds/Sos_boludo_y_no_tenes_huevos.wav");
+        }if(e.getSource() == boton6){
+        	ReproducirSonido("sounds/Ese_es_el_equilatero_idiota.wav");
+        }if(e.getSource() == boton7){
+        	ReproducirSonido("sounds/Puede_darme_dinero.wav");
+        }if(e.getSource() == boton8){
+        	ReproducirSonido("sounds/Jajaja_Basta_Chicos.wav");
+        }if(e.getSource() == boton9){
+        	ReproducirSonido("sounds/Aguantaaaa.wav");
         }
     }
 }
